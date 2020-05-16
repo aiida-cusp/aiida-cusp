3a25a9cc962fe078e540c0e76b7a2755d8868d44
A. Stamminger <andreas.stamminger@pm.me>
2020-05-16 13:01:13 +0200

    Add parametrization to regular expressions tests
    All tests for regular expressions used in the PotcarParser class are now
    executed from parametrized lists. This makes it easier to subsequently extend
    the available list of performed test cases with additional cases covering
    tests of the regexes against mean patterns that may pop up over time.

--- tests/utils/potcar_test.py
+++ tests/utils/potcar_test.py
@@ -6,13 +6,165 @@ Test suite for POTCAR related utilities
 """
 
 
+import pytest
+import string
+
+from pymatgen.core import periodic_table
+
 from aiida_cusp.utils import PotcarParser
 
 
-def test_header_parsing(interactive_potcar_file):
-    # fictiious potential file header
+# ever growing list of sample cases testing the regular expression used
+# to remove certain chars from the contents. every case that lead to a bug
+# should be added here!
+@pytest.mark.parametrize('sample_input,expected_string',
+[   # noqa: E128
+    ("^", ""),
+    ("^^", ""),
+    ("^^^", ""),
+    ("^^^^", ""),
+    ("^sample", "sample"),
+    ("sample^", "sample"),
+    ("^^^sample", "sample"),
+    ("sample^^^", "sample"),
+    ("s^a^m^p^l^e", "sample"),
+    ("sa^^m^^^p^le", "sample"),
+])
+def test_remove_regex(sample_input, expected_string):
+    remove_regex = PotcarParser._RE_REMOVE_CHARS
+    cleared_string = remove_regex.sub(r"", sample_input)
+    assert cleared_string == expected_string
+
+
+# ever growing list of sample cases testing the regular expression used
+# to condense certain consecutive chars (i.e. whitespaces) in inputs. every
+# case that lead to a bug should be added here!
+@pytest.mark.parametrize('sample_input,expected_string',
+[   # noqa: E128
+    # simple empty space
+    (" ", " "),
+    ("  ", " "),
+    ("   ", " "),
+    ("    ", " "),
+    (" sample", " sample"),
+    ("   sample", " sample"),
+    ("sample ", "sample "),
+    ("sample   ", "sample "),
+    ("s  a   m p l    e", "s a m p l e"),
+    # tabspace
+    ("\t", " "),
+    ("\t\t", " "),
+    ("\t\t\t", " "),
+    ("\t\t\t\t", " "),
+    ("\tsample", " sample"),
+    ("\t\t\tsample", " sample"),
+    ("sample\t", "sample "),
+    ("sample\t\t\t", "sample "),
+    ("s\t\ta\t\t\tm\tp\tl\t\t\t\te", "s a m p l e"),
+    # mixed empty and tabspaces
+    ("s\t \ta\t  \t\tm\tp\t  l \t \t\t \te", "s a m p l e"),
+])
+def test_reduced_regex(sample_input, expected_string):
+    remove_regex = PotcarParser._RE_CONDENSE_CHARS
+    reduced_string = remove_regex.sub(r" ", sample_input)
+    assert reduced_string == expected_string
+
+
+# ever growing list of sample case testing the header machting regular
+# expression. expected match should be everything encompassed by %HSTRT%
+# and %HEND%. every case that lead to a bug should be added here!
+@pytest.mark.parametrize('sample_input,expected_match',
+[   # noqa: E128
+    # check matching is working independent of surrounding contents
+    ("before head %HSTRT% header %HEND%", " header "),
+    ("%HSTRT% header %HEND% after head", " header "),
+    ("before head %HSTRT% header %HEND% after head", " header "),
+    ("before header\n%HSTRT% header %HEND%\nafter header", " header "),
+    ("%HSTRT%\nhe\nad\ner\n%HEND%", "\nhe\nad\ner\n"),
+    ("before header%HSTRT%header%HEND%after header", "header"),
+    # actually check that the regex is able to match **everything**
+    ("%HSTRT%" + string.printable + "%HEND%", string.printable),
+])
+def test_header_regex(sample_input, expected_match):
+    header_regex = PotcarParser._RE_HEADER
+    # replace placeholder with actual start / stop identifiers used by VASP
+    head_start_ident = "parameters from PSCTR are:"
+    head_end_ident = "END of PSCTR-controll parameters"
+    sample_input = sample_input.replace("%HSTRT%", head_start_ident)
+    sample_input = sample_input.replace("%HEND%", head_end_ident)
+    matched_header = header_regex.search(sample_input).group(1)
+    assert matched_header == expected_match
+
+
+# define different possible ocurrences of the VRHFIN line inside the
+# potential file header and test matching again all element strings available
+# from the pymatgen.core.periodic_table module. (%E% will be replaced by the
+# element name during the test)
+@pytest.mark.parametrize('sample_input',
+[   # noqa: E128
+    (string.printable + "VRHFIN=%E%:" + string.printable),
+    (string.printable + " VRHFIN=%E%:" + string.printable),
+    (string.printable + "   VRHFIN=%E%:" + string.printable),
+    (string.printable + "VRHFIN=%E% :" + string.printable),
+    (string.printable + "VRHFIN=%E%   :" + string.printable),
+    (string.printable + "VRHFIN =%E%:" + string.printable),
+    (string.printable + "VRHFIN    =%E%:" + string.printable),
+    (string.printable + "VRHFIN = %E%:" + string.printable),
+    (string.printable + "VRHFIN =   %E%:" + string.printable),
+    (string.printable + "VRHFIN =%E% :" + string.printable),
+    (string.printable + "VRHFIN =%E%   :" + string.printable),
+    (string.printable + "VRHFIN =  %E%  :" + string.printable),
+])
+def test_element_regex(sample_input):
+    element_regex = PotcarParser._RE_ELEMENT
+    element_list = [e.name for e in periodic_table.Element]
+    for wanted_element in element_list:
+        vrhfin_line = sample_input.replace("%E%", wanted_element)
+        parsed_element = element_regex.search(vrhfin_line).group(1)
+        assert parsed_element == wanted_element
+
+
+# the regex for date matching is not supposed to match a valid date but rather
+# match for strings that **look like** a valid date used by VASP, however, it
+# should certainly not match anything that looks like a electron configuration,
+# i.e. s2p6, ...
+@pytest.mark.parametrize('sample_input,expected_match',
+[   # noqa: E128
+    # every three letter structure surrounded by at least a single digit
+    # is valid
+    (string.printable + " 99Zyx99 " + string.printable, "99Zyx99"),
+    (string.printable + " 99Zyx9999 " + string.printable, "99Zyx9999"),
+    (string.printable + " 9Zyx9999 " + string.printable, "9Zyx9999"),
+    (string.printable + " 9Zyx9 " + string.printable, "9Zyx9"),
+    (string.printable + " 99999Zyx99999 " + string.printable, "99999Zyx99999"),
+    # everything else should produce not match to avoid false positive matches
+    # for electron configurations
+    (string.printable + " Zyx9 " + string.printable, None),
+    (string.printable + " 9Zyx " + string.printable, None),
+    (string.printable + " 9Zy9 " + string.printable, None),
+    (string.printable + " Zy9 " + string.printable, None),
+    (string.printable + " 9Zb " + string.printable, None),
+    (string.printable + " 9Z9 " + string.printable, None),
+    (string.printable + " Z9 " + string.printable, None),
+    (string.printable + " z9y9 " + string.printable, None),
+    (string.printable + " z99y999 " + string.printable, None),
+    (string.printable + " z9y9x9 " + string.printable, None),
+])
+def test_date_regex(sample_input, expected_match):
+    date_regex = PotcarParser._RE_DATE
+    parsed_date = date_regex.search(sample_input)
+    if expected_match is None:
+        assert parsed_date is None
+    else:
+        assert parsed_date.group(1) == expected_match
+
+
+# regression testing for the PotcarParser when parsing the contents of a
+# *real* potential file
+def test_potcar_parser_class(interactive_potcar_file):
+    # fictitious potential file header
     potential_head = (
-        "Functional Ee 99Mar9999 10.0000000000000000\n"
+        "Functional  99Mar9999 10.0000000000000000\n"
         "parameters from PSCTR are:\n"
         "  VRHFIN =Ee: d10 p1\n"
         "  LEXCH  = PE\n"
@@ -21,7 +173,8 @@ def test_header_parsing(interactive_potcar_file):
         "  TITEL  = PAW_PBE Cu 05Jan2001\n"
         "  LULTRA =        F    use ultrasoft PP ?\n"
         "  IUNSCR =        1    unscreen: 0-lin 1-nonlin 2-no\n"
-        "END of PSCTR-controll parameters"
+        "END of PSCTR-controll parameters\n"
+        "numerical potential contents following the header"
     )
     interactive_potcar_file.open("POTCAR")
     interactive_potcar_file.write(potential_head)
@@ -31,65 +184,53 @@ def test_header_parsing(interactive_potcar_file):
     assert parsed.version == 99990399
 
 
-def test_remove_regex():
-    remove_regex = PotcarParser._RE_REMOVE_CHARS
-    test_cases = ["^", "\t"]
-    for test_case in test_cases:
-        removed = remove_regex.sub(r"", test_case)
-        assert removed == ""
-
-
-def test_reduce_regex():
-    reduce_regex = PotcarParser._RE_CONDENSE_CHARS
-    test_cases = ["  ", "   ", "    "]
-    for test_case in test_cases:
-        reduced = reduce_regex.sub(r" ", test_case)
-        assert reduced == " "
-
-
-def test_header_regex():
-    header_regex = PotcarParser._RE_HEADER
-    test_case = (
-        "Title line\n"
-        "0.0000000000000000\n"
-        "parameters from PSCTR are:\n"
-        "random header contents\n"
-        "END of PSCTR-controll parameters\n"
-    )
-    matched_header = header_regex.search(test_case).group(1)
-    assert matched_header == "\nrandom header contents\n"
-
-
-def test_element_regex():
-    import random
-    from pymatgen.core import periodic_table
-    regex_element = PotcarParser._RE_ELEMENT
-    test_case = "VRHFIN{}={}{}{}:continued"
-    # test regex against all elements stored in pymatgen
-    for item in periodic_table.Element:
-        element = item.name
-        # add some thrill by including some random whitespaces
-        w1 = random.randint(0, 20) * " "
-        w2 = random.randint(0, 20) * " "
-        w3 = random.randint(0, 20) * " "
-        test_string = test_case.format(w1, w2, element, w3)
-        element_match = regex_element.search(test_string).group(1)
-        assert element_match == element
-
-
-def test_date_regex():
-    regex_date = PotcarParser._RE_DATE
-    # these cases should produce a match
-    test_case = "9Abc99"
-    date_match = regex_date.search(test_case).group(1)
-    assert date_match == test_case
-    test_case = "99Abc9999"
-    date_match = regex_date.search(test_case).group(1)
-    assert date_match == test_case
-    # these cases should produce not match
-    test_case = "Abc99"
-    assert regex_date.search(test_case) is None
-    test_case = "99Abc"
-    assert regex_date.search(test_case) is None
-    test_case = "Abc"
-    assert regex_date.search(test_case) is None
+# test application of implemented quirks
+@pytest.mark.parametrize('quirk,expected_attributes',
+[   # noqa: E128
+    ('PAW_PBE Xe 07Sep2000', {
+        'element': 'Xe',
+    })
+    ('PAW Xe 07Sep2000', {
+        'element': 'Xe',
+    })
+    ('PAW_PBE Zr_sv 04Jan2005', {
+        'element': 'Zr',
+    })
+    ('US Bi', {
+        'element': 'Bi',
+    })
+    ('PAW Bi_pv 29Jan08', {
+        'version': 20080129,
+    })
+    ('PAW Ga_d_GW 10Nov06', {
+        'version': 20061110,
+    })
+    ('PAW_PBE Bi_pv 29Jan08', {
+        'version': 20080129,
+    })
+    ('PAW_PBE Bi_pv 29Jan08 GW ready', {
+        'version': 20080129,
+    })
+    ('PAW_PBE Ga_sv_GW 03Mar08', {
+        'version': 20080303,
+    })
+])
+def test_quirks_are_applied(interactive_potcar_file, quirk,
+                            expected_attributes):
+    # construct potential with appropriate title line to trigger the
+    # quirk execution
+    potential_contents = "\n".join([
+        "{} VRHFIN =Xy: s100p100d100".format(quirk),
+        "9.999999999999999999999",
+        "parameters from PSCTR are:",
+        "VRHFIN =Xy: s100p100d100",
+        "TITEL  = {}".format(quirk),
+        "END of PSCTR-controll parameters",
+        "numerical potential contents following the header",
+    ])
+    interactive_potcar_file.open("POTCAR")
+    interactive_potcar_file.write(potential_contents)
+    path_to_potcar = interactive_potcar_file.filepath
+    parsed_potential = PotcarParser(path_to_potcar)
+    for (attr_name, expected_attr_val) in expected_attributes.items():
+        assert getattr(parsed_potential, attr_name) == expected_attr_val

43bbe31ad8bcbe4379912e31d6f851dcbef6ba9e
A. Stamminger <andreas.stamminger@pm.me>
2020-05-25 19:07:45 +0200

    Implement datatype for VASP incar files

---
+++ aiida_cusp/data/inputs/vasp_incar.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Datatype and methods to initialize and interact with VASP specific INCAR
+input data
+"""
+
+
+from aiida.orm import Dict
+from pymatgen.io.vasp.inputs import Incar
+
+
+class IncarWrapperError(Exception):
+    """Exception raised by the Incar wrapper class."""
+    pass
+
+
+class VaspIncarData(Dict):
+    """
+    VaspIncarData(incar=None)
+
+    AiiDA compatible node representing a VASP incar data object based on the
+    :class:`~pymatgen.io.vasp.inputs.Incar` datatype.
+
+    :param incar: input parameters used to construct the :class:`~pymatgen.\
+        io.casp.inputs.Incar` object or a incar object itself (Note: may also
+        be set to `None` to initialize an empty incar object and use the
+        VASP default parameters)
+    :type incar: dict or :class:`~pymatgen.io.vasp.inputs.Incar`
+    """
+    def __init__(self, *args, **kwargs):
+        # if incar is set: assume initialization from user space. Cannot
+        # use None here since it is a valid value for incar
+        incar = kwargs.pop('incar', False)
+        if not incar:
+            super(VaspIncarData, self).__init__(*args, **kwargs)
+        else:  # redirect to wrapper if incar is set
+            incar = IncarWrapper(incar=incar)
+            super(VaspIncarData, self).__init__(dict=incar.as_dict())
+
+    def get_incar(self):
+        """
+        get_incar()
+
+        Create and return a :class:`~pymatgen.io.vasp.inputs.Incar` instance
+        initialized from the node's stored incar data contents.
+
+        :return: a pymatgen Incar data instance
+        :rtype: :class:`pymatgen.io.vasp.inputs.Incar`
+        """
+        return Incar.from_dict(self.get_dict())
+
+    def write_file(self, filename):
+        """
+        write_file(filename)
+
+        Write the stored incar data to VASP input file.
+
+        Output of the contents to the file is redirected to the
+        :meth:`pymatgen.io.vasp.inputs.Incar.write_file` method and the created
+        output file will be formatted as VASP input file (INCAR)
+
+        :param filename: destination for writing the output file
+        :type filename: str
+        :return: None
+        """
+        incar = self.get_incar()
+        incar.write_file(filename)
+
+
+class IncarWrapper(object):
+    """
+    Utility class for initializing :class:`pymatgen.io.vasp.inputs.Incar`
+    data objects
+
+    Accepts either a :class:`~pymatgen.io.vasp.inputs.Incar instance or
+    a dictionary containing valid VASP Incar parameters which will be passed
+    through to the :class:`~pymatgen.io.vasp.inputs.Incar` constructor. Note:
+    If `incar` is set to `None` and empty incar file will be initialized.
+
+    :param incar: input parameters used to construct the :class:`~pymatgen.\
+        io.casp.inputs.Incar` object or a incar object itself.
+    :type incar: dict or :class:`~pymatgen.io.vasp.inputs.Incar`
+    """
+
+    def __new__(cls, incar=None):
+        # check if already pymatgen Incar instance
+        if isinstance(incar, Incar):
+            return incar
+        elif isinstance(incar, dict):  # initialize from user input
+            incar_params_upper = cls.keys_to_upper_case(incar)
+            incar_data = Incar(params=incar_params_upper)
+        elif incar is None:
+            incar_data = Incar(params=None)
+        else:
+            raise IncarWrapperError("Unknown type '{}' for incar parameters"
+                                    .format(type(incar)))
+        return incar_data
+
+    @classmethod
+    def keys_to_upper_case(cls, rawdict):
+        """
+        Transform all incar parameter keys to upper case
+
+        :param rawdict: input dictionary containing incar parameter key
+            value pairs with keys possibly of mixed case
+        :type rawdict: dict
+        """
+        return {key.upper(): value for (key, value) in rawdict.items()}
---
+++ tests/data/inputs/incar_test.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Test suite for the VaspIncarData and the related IncarWrapper utility
+"""
+
+
+import pytest
+
+import pathlib
+
+from pymatgen.io.vasp.inputs import Incar
+
+from aiida_cusp.data.inputs.vasp_incar import IncarWrapper, VaspIncarData
+from aiida_cusp.data.inputs.vasp_incar import IncarWrapperError
+
+
+#
+# Tests for IncarWrappper
+#
+def test_empty_input():
+    incar_parameters = None
+    incar = IncarWrapper(incar=incar_parameters)
+    assert str(incar).strip() == ""
+
+
+def test_init_from_incar_object():
+    incar_parameters = {'ISMEAR': 0, 'SIGMA': 5.0E-2}
+    pmg_incar = Incar(params=incar_parameters)
+    incar = IncarWrapper(incar=pmg_incar)
+    assert str(pmg_incar) == str(incar)
+
+
+def test_init_from_user_dict():
+    incar_parameters = {'ISMEAR': 0, 'SIGMA': 5.0E-2}
+    incar = IncarWrapper(incar=incar_parameters)
+    pmg_incar = Incar(params=incar_parameters)
+    assert str(incar) == str(pmg_incar)
+
+
+@pytest.mark.parametrize('incar_param_type', [list(), tuple(), float, int])
+def test_unknown_input_type_raises(incar_param_type):
+    with pytest.raises(IncarWrapperError) as exception:
+        incar = IncarWrapper(incar=incar_param_type)
+    assert "Unknown type" in str(exception.value)
+
+
+def test_keys_to_upper_case():
+    mixed_case_dict = {'iSMeaR': 0, 'sigma': 0.05, 'ENCUT': 100}
+    upper_case = IncarWrapper.keys_to_upper_case(mixed_case_dict)
+    assert 'ISMEAR' in upper_case.keys()
+    assert 'SIGMA' in upper_case.keys()
+    assert 'ENCUT' in upper_case.keys()
+
+
+#
+# Test for VaspIncarData
+#
+def test_get_incar_method():
+    incar_parameters = {
+        'EDIFF': 1.0E-6,
+        'EDIFFG': -0.001,
+        'ISMEAR': 0,
+        'SIGMA': 0.01,
+    }
+    incar = VaspIncarData(incar=incar_parameters)
+    pmg_incar = Incar(params=incar_parameters)
+    assert str(incar.get_incar()) == str(pmg_incar)
+
+
+def test_store_and_load_incar_data(clear_database_before_test):
+    from aiida.orm import load_node
+    incar_parameters = {
+        'EDIFF': 1.0E-6,
+        'EDIFFG': -0.001,
+        'ISMEAR': 0,
+        'SIGMA': 0.01,
+    }
+    incar_set = VaspIncarData(incar=incar_parameters)
+    uuid = incar_set.store().uuid
+    incar_get = load_node(uuid)
+    assert str(incar_set.get_incar()) == str(incar_get.get_incar())
+
+
+def test_write_file_method(tmpdir):
+    incar_parameters = {
+        'EDIFF': 1.0E-6,
+        'EDIFFG': -0.001,
+        'ISMEAR': 0,
+        'SIGMA': 0.01,
+    }
+    incar = VaspIncarData(incar=incar_parameters)
+    filepath = pathlib.Path(tmpdir) / 'INCAR'
+    assert filepath.is_file() is False  # assert file is not already there
+    incar.write_file(filepath)
+    assert filepath.is_file() is True  # assert file has been written
+    # load contents from file and compare to node contents
+    with open(filepath, 'r') as stored_incar_file:
+        written_contents = stored_incar_file.read()
+    assert written_contents == str(incar.get_incar())

934a7aea1759f9cca5a82065e481b4ec7d987e20
A. Stamminger <andreas.stamminger@pm.me>
2020-06-03 18:52:04 +0200

    Make functional map case insensitive
    Functionals are now identified and mapped to the internal functional
    types in a case insensitive manner.

--- aiida_cusp/utils/defaults.py
+++ aiida_cusp/utils/defaults.py
@@ -6,19 +6,24 @@ class VaspDefaults(object):
     # map functionals contained in archive file names to internal string
     FUNCTIONAL_MAP = {
         # LDA type potentials
-        'potUSPP_LDA': 'lda_us',
-        'potpaw_LDA': 'lda',
-        'potpaw_LDA.52': 'lda_52',
-        'potpaw_LDA.54': 'lda_54',
+        'potuspp_lda': 'lda_us',
+        'potpaw_lda': 'lda',
+        'potpaw_lda.52': 'lda_52',
+        'potpaw_lda.54': 'lda_54',
         # PBE type potentials
-        'potpaw_PBE': 'pbe',
-        'potpaw_PBE.52': 'pbe_52',
-        'potpaw_PBE.54': 'pbe_54',
+        'potpaw_pbe': 'pbe',
+        'potpaw_pbe.52': 'pbe_52',
+        'potpaw_pbe.54': 'pbe_54',
         # PW91 type potentials
-        'potUSPP_GGA': 'pw91_us',
-        'potpaw_GGA': 'pw91',
+        'potuspp_gga': 'pw91_us',
+        'potpaw_gga': 'pw91',
     }
     # filenames for VASP input and output files
     FNAMES = {
         'potcar': 'POTCAR',
+        'incar': 'INCAR',
+        'poscar': 'POSCAR',
+        'contcar': 'CONTCAR',
+        'potcar': 'POTCAR',
+        'kpoints': 'KPOINTS',
     }

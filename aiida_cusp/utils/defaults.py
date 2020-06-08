ff1dc2d4213527c1ed83710ba74aa25c382fe8b2
A. Stamminger <andreas.stamminger@pm.me>
2020-06-08 19:26:10 +0200

    Add defaults for Custodian

--- aiida_cusp/utils/defaults.py
+++ aiida_cusp/utils/defaults.py
@@ -64,3 +64,8 @@ class PluginDefaults(object):
     # filenames for logging of stdin and stderr during AiiDA VASP calculations
     STDERR_FNAME = 'aiida.err'
     STDOUT_FNAME = 'aiida.out'
+
+
+class CustodianDefaults(object):
+    # default name used for the input file to the cstdn executable
+    SPEC_FNAME = 'cstdn_spec_file.yaml'

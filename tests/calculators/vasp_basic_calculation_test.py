e27b91f50d45068174a4d92eb96aeb46712b229e
A. Stamminger <andreas.stamminger@pm.me>
2020-06-13 14:51:12 +0200

    Add testsuite for the VaspBasicCalculator plugin

--- tests/calculators/vasp_basic_calculation_test.py
+++ tests/calculators/vasp_basic_calculation_test.py
@@ -4,13 +4,6 @@
 import pytest
 
 
-# TODO: Test restart with poscar overwrite ON / OFF (simply check the copy list
-#       in the calcinfo because actualy copying is already tested)
-# TODO: Test invalid inputs raise on restart (No copy needed)
-# TODO: Test defined inputs are preferred (once again simply check the copy
-#       list and possibly the list of excluded files as well)
-
-
 @pytest.mark.parametrize('valid_input', ['incar', 'kpoints', 'poscar',
                          'potcar', 'restart'])
 def test_input_port_availability(valid_input):
@@ -103,3 +96,124 @@ def test_vasp_calculation_setup(vasp_code, cstdn_code, incar, kpoints, poscar,
     # assert the specfile was written to the folder
     c = pathlib.Path(aiida_sandbox.abspath) / PluginDefaults.CSTDN_SPEC_FNAME
     assert c.is_file() is True
+
+
+@pytest.mark.parametrize('invalid_input', ['poscar', 'potcar'])
+def test_invalid_restart_inputs_raise(vasp_code, poscar, with_pbe_potcars,
+                                      invalid_input):
+    from aiida.plugins import CalculationFactory
+    from aiida_cusp.data import VaspPotcarData
+    VaspBasicCalculation = CalculationFactory('cusp.vasp')
+    inputs = {
+        'code': vasp_code,
+        'metadata': {
+            'options': {
+                'resources': {'num_machines': 1},
+            },
+        },
+    }
+    if invalid_input == 'poscar':
+        inputs.update({'poscar': poscar})
+    if invalid_input == 'potcar':
+        potcar = VaspPotcarData.from_structure(poscar, 'pbe')
+        inputs.update({'potcar': potcar})
+    vasp_basic_calculation = VaspBasicCalculation(inputs=inputs)
+    with pytest.raises(Exception) as exception:
+        vasp_basic_calculation.verify_restart_inputs()
+    err_msg = "the following defined inputs are not allowed in a restarted"
+    assert err_msg in str(exception.value)
+
+
+@pytest.mark.parametrize('switch', [True, False])
+def test_poscar_overwrite_switch(switch, tmpdir, vasp_code, aiida_sandbox):
+    import pathlib
+    from aiida.orm import RemoteData
+    from aiida.plugins import CalculationFactory
+    from aiida_cusp.data import VaspPotcarData
+    # set the input plugin for code
+    vasp_code.set_attribute('input_plugin', 'cusp.vasp')
+    # setup a remote restart directory with POSCAR and CONTCAR
+    computer = vasp_code.computer
+    pathlib.Path(tmpdir / 'POSCAR').touch()
+    pathlib.Path(tmpdir / 'CONTCAR').touch()
+    remote_path = str(tmpdir)
+
+    remote_data = RemoteData(computer=computer, remote_path=remote_path)
+    VaspBasicCalculation = CalculationFactory('cusp.vasp')
+    inputs = {
+        'code': vasp_code,
+        'restart': {
+            'folder': remote_data,
+            'contcar_to_poscar': switch,
+        },
+        'metadata': {
+            'options': {
+                'resources': {'num_machines': 1},
+            },
+        },
+    }
+    vasp_basic_calculation = VaspBasicCalculation(inputs=inputs)
+    calcinfo = vasp_basic_calculation.prepare_for_submission(aiida_sandbox)
+    remote_copy_list = calcinfo.remote_copy_list
+    copied_files = [pathlib.Path(f).name for (_, f, _) in remote_copy_list]
+    # the contcar file will always be copied no matter if the switch is set
+    # or not
+    assert 'CONTCAR' in copied_files
+    # now check if CONTCAR is copied on itself or on the new POSCAR
+    for (uuid, abspath_remote, relpath_input) in remote_copy_list:
+        filename_remote = pathlib.Path(abspath_remote).name
+        filename_input = pathlib.Path(relpath_input).name
+        if filename_remote == 'CONTCAR':
+            if switch:  # True: CONTCAR --> POSCAR
+                assert filename_input == 'POSCAR'
+                assert 'POSCAR' not in copied_files
+            else:  # False: CONTCAR --> CONTCAR
+                assert filename_input == 'CONTCAR'
+                assert 'POSCAR' in copied_files
+
+
+@pytest.mark.parametrize('use_incar', [False, True])
+@pytest.mark.parametrize('use_kpoints', [False, True])
+def test_defined_inputs_are_preferred(use_incar, use_kpoints, tmpdir,
+                                      vasp_code, aiida_sandbox, incar,
+                                      kpoints):
+    import pathlib
+    from aiida.orm import RemoteData
+    from aiida.plugins import CalculationFactory
+    from aiida_cusp.data import VaspPotcarData
+    # set the input plugin for code
+    vasp_code.set_attribute('input_plugin', 'cusp.vasp')
+    # setup a remote restart directory with POSCAR and CONTCAR
+    computer = vasp_code.computer
+    pathlib.Path(tmpdir / 'INCAR').touch()
+    pathlib.Path(tmpdir / 'KPOINTS').touch()
+    remote_path = str(tmpdir)
+    remote_data = RemoteData(computer=computer, remote_path=remote_path)
+    VaspBasicCalculation = CalculationFactory('cusp.vasp')
+    inputs = {
+        'code': vasp_code,
+        'restart': {
+            'folder': remote_data,
+        },
+        'metadata': {
+            'options': {
+                'resources': {'num_machines': 1},
+            },
+        },
+    }
+    if use_incar:
+        inputs.update({'incar': incar})
+    if use_kpoints:
+        inputs.update({'kpoints': kpoints})
+    vasp_basic_calculation = VaspBasicCalculation(inputs=inputs)
+    calcinfo = vasp_basic_calculation.prepare_for_submission(aiida_sandbox)
+    remote_copy_list = calcinfo.remote_copy_list
+    files_to_copy = [pathlib.Path(f).name for (_, f, _) in remote_copy_list]
+    if use_incar:  # incar input defined: do not copy
+        assert 'INCAR' not in files_to_copy
+    else:
+        assert 'INCAR' in files_to_copy
+    if use_kpoints:  # kpoint input defined: do not copy
+        assert 'KPOINTS' not in files_to_copy
+    else:
+        assert 'KPOINTS' in files_to_copy

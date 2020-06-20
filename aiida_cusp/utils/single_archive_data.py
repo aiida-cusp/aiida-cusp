ac624a4437251da56d6f5d49a4c2a7450409590a
A. Stamminger <andreas.stamminger@pm.me>
2020-06-20 12:20:42 +0200

    Add utility class extending AiiDA's SinglefileData class
    This commit adds the new utility SingleArchiveData class. It naturally
    extends the AiiDA internal SinglefileData class to store the given file
    as gzip-compressed archive rather than storing the plain file. Expecially
    for VASP with its plain-text outputs storing the files via the newly
    implemented class offers huge advantages regarding the occupied disk
    space of the repository.
    
    This has been testes for an exemplatory vasprun.xml obtained from
    a performed AIMD simulation using VASP. Here, the size of the originally
    stored vasprun.xml file was 527 MB before compression and 117 MB after
    the compression using gzip (reduction in size by a factor of ~4).
    Likely even higher compression ratios may be found for the plain text
    files like OUTCARs etc.

---
+++ aiida_cusp/utils/single_archive_data.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Extension to AiiDA's SinglefileData storing the given file as gzip-compressed
+archive instead of simply storing the file as-is
+"""
+
+
+import pathlib
+import gzip
+import io
+
+from aiida.orm import SinglefileData
+
+
+class SingleArchiveData(SinglefileData):
+    """
+    Data class used to store the contents of a file as gzip compressed
+    archive in its repository
+    """
+
+    # suffix appended to the file name deduced from the given path when stored
+    # to the repository
+    ARCHIVE_SUFFIX = '.gz'
+
+    def set_file(self, file, filename=None):
+        """
+        Compress given file and store it to the node's repository.
+
+        This method differs from the original SinglefileData.set_file()
+        method in that it only accepts a filepath. The additional filename
+        argument is only here to make AiiDA happy and is ignored since we
+        only allow file path inputs from which the filename is deduced.
+
+        :param file: path to the file whose contents will be compressed and
+            stored to the repository
+        :type file: pathlib.Path
+        :raises ValueError: if the given path does not point to a file
+        """
+        # check if the path points to a valid file
+        if not file.is_file():
+            raise ValueError("given file path '{}' does not point to a file"
+                             .format(file.absolute()))
+        # get the compressed contents as io.BytesIO and initialize the
+        # SinglefileData node using the byte stream
+        file, filename = self.get_compressed_file_contents(file)
+        super(SingleArchiveData, self).set_file(file=file, filename=filename)
+
+    def get_compressed_file_contents(self, filepath):
+        """
+        Load the file and return a gzip compressed version of the contents
+
+        :param filepath: path to the file whose contents will be loaded
+            and compressed using gzip
+        :type filepath: pathlib.Path
+        :returns: a tuple containing the compressed file contents and the
+            file's name with '.gz' suffix appended
+        :rtype: tuple
+        """
+        # transform to Path() object (nothing happens if it already is)
+        filepath = pathlib.Path(filepath).absolute()
+        with open(filepath, 'rb') as infile:
+            compressed_contents = gzip.compress(infile.read())
+        filepath = filepath.with_suffix(filepath.suffix + self.ARCHIVE_SUFFIX)
+        # AiiDA excpects a BytesIO object to initialize the node from stream
+        return io.BytesIO(compressed_contents), str(filepath.name)
+
+    def get_content(self, decompress=True):
+        """
+        Load the node and return a either the archive or decompressed contents
+        stored in the node.
+
+        :param decompress: Indicate wether comressed or uncompressed contents
+            are returned
+        :type decompress: `bool`
+        """
+        with self.open(mode='rb') as archive:
+            archive_contents = archive.read()
+        if decompress:
+            return gzip.decompress(archive_contents)
+        else:
+            return archive_contents
+
+    # TODO: Considering the repository redesign I have no idead if this will
+    #       continiue to work in the future (Although only Repository internal
+    #       and no external methods are applied to get the file path
+    def get_file_path(self):
+        """
+        Return the path of the object in the repository.
+
+        Basically replicates the procedure in the internal open method without
+        the final call to the open()
+        """
+        # get the path to the file stored in the repository
+        return self._repository._get_base_folder().get_abs_path(self.filename)
---
+++ tests/utils/single_archive_data_test.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Test suite for the SingleArchiveData class
+"""
+
+
+import pytest
+
+import pathlib
+import gzip
+
+
+def test_store_and_load_archive_data(tmpdir):
+    from aiida_cusp.utils.single_archive_data import SingleArchiveData
+    from aiida.orm import load_node
+    testfile = pathlib.Path(tmpdir / 'testfile.txt')
+    testcontent = "Test file contents".encode()
+    # write contents to the testfile
+    with open(testfile, 'wb') as filehandle:
+        filehandle.write(testcontent)
+    # store and re-load SingleArchiveData and compare the contents
+    # in compressed format
+    single_archive = SingleArchiveData(file=testfile)
+    uuid = single_archive.store().uuid
+    loaded_single_archive = load_node(uuid)
+    assert single_archive.get_content() == loaded_single_archive.get_content()
+
+
+def test_single_archive_node_contents(tmpdir):
+    from aiida_cusp.utils.single_archive_data import SingleArchiveData
+    testfile = pathlib.Path(tmpdir / 'testfile.txt')
+    testcontent = "Test file contents".encode()
+    testcontent_compressed = gzip.compress(testcontent)
+    # write contents to the testfile
+    with open(testfile, 'wb') as filehandle:
+        filehandle.write(testcontent)
+    # init SingleArchiveData from the testfile and check contents are stored
+    # in compressed format
+    single_archive = SingleArchiveData(file=testfile)
+    with open(single_archive.get_file_path(), 'rb') as filehandle:
+        node_contents = filehandle.read()
+    assert node_contents == testcontent_compressed
+
+
+@pytest.mark.parametrize('decompress', [True, False])
+def test_get_content_method(tmpdir, decompress):
+    from aiida_cusp.utils.single_archive_data import SingleArchiveData
+    testfile = pathlib.Path(tmpdir / 'testfile.txt')
+    testcontent = "Test file contents".encode()
+    testcontent_compressed = gzip.compress(testcontent)
+    with open(testfile, 'wb') as filehandle:
+        filehandle.write(testcontent)
+    # init SingleArchiveData from the testfile and check contents are stored
+    single_archive = SingleArchiveData(file=testfile)
+    contents = single_archive.get_content(decompress=decompress)
+    if decompress:
+        assert contents == testcontent
+    else:
+        assert contents == testcontent_compressed
+
+
+def test_get_repository_file_path(tmpdir):
+    from aiida_cusp.utils.single_archive_data import SingleArchiveData
+    testfile = pathlib.Path(tmpdir / 'testfile.txt')
+    testcontent = "Test file contents".encode()
+    testcontent_compressed = gzip.compress(testcontent)
+    with open(testfile, 'wb') as filehandle:
+        filehandle.write(testcontent)
+    single_archive = SingleArchiveData(file=testfile)
+    node_path = single_archive.get_file_path()
+    with open(node_path, 'rb') as filehandle:
+        content_at_path = filehandle.read()
+    assert content_at_path == testcontent_compressed

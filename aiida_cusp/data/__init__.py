ed30caa5f7f7f319a8edac05079e993f670b3c51
A. Stamminger <andreas.stamminger@pm.me>
2020-06-21 20:27:06 +0200

    Promote output datatypes to the aiida_cusp.data namespace

--- aiida_cusp/data/__init__.py
+++ aiida_cusp/data/__init__.py
@@ -5,7 +5,13 @@ from .inputs.vasp_kpoint import VaspKpointData
 from .inputs.vasp_poscar import VaspPoscarData
 from .inputs.vasp_potcar import VaspPotcarData
 from .inputs.vasp_incar import VaspIncarData
+from .outputs.vasp_vasprun import VaspVasprunData
+from .outputs.vasp_contcar import VaspContcarData
+from .outputs.vasp_outcar import VaspOutcarData
+from .outputs.vasp_chgcar import VaspChgcarData
+from .outputs.vasp_wavecar import VaspWavecarData
 
 
 __all__ = ["VaspKpointData", "VaspPoscarData", "VaspIncarData",
-           "VaspPotcarData"]
+           "VaspPotcarData", "VaspVasprunData", "VaspContcarData",
+           "VaspOutcarData", "VaspChgcarData", "VaspWavecarData"]
